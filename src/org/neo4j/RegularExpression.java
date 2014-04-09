c394d8515f55d3c6ff24caf03e85fcdb56933ab8
Rohit Zawar <zawar.rohit@gmail.com>
2014-04-09 03:49:48 -0400

    IMplemented the JSON file ouptput for selected message ID's.

--- src/org/neo4j/RegularExpression.java
+++ src/org/neo4j/RegularExpression.java
@@ -1,19 +1,51 @@
 package org.neo4j;
 
-import java.io.*;
-import java.text.*;
-import java.util.*;
-import org.neo4j.graphdb.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.ws.rs.core.MediaType;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.index.Index;
 import org.neo4j.graphdb.index.IndexManager;
 import org.neo4j.graphdb.index.RelationshipIndex;
 import org.neo4j.helpers.collection.MapUtil;
 
+import scala.util.parsing.json.JSON;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+
 public class RegularExpression {
 
 	GraphDatabaseService service;
-	private static final String path="D:\\Check73.graphdb";
+	private static final String path="D:\\Check88.graphdb";
 	IndexManager indexManager;
 	Index<Node> userIndex, tweetIndex, hashTagIndex;
 	RelationshipIndex relIndex;
@@ -21,7 +53,7 @@ public class RegularExpression {
 	 * @param args
 	 */
 	private static enum RelationType implements RelationshipType{
-		TWEETS, RETWEETS, CONTAINS, ISARETWEETOF, MENTIONS, ISAREPLYTOTWEET,REPLIES;
+		TWEETS, RETWEETS, CONTAINS, IS_A_RETWEETOF, MENTIONS, IS_A_REPLYTOTWEET,REPLIES;
 	}
 	public RegularExpression() {
 		// TODO Auto-generated constructor stub
@@ -55,8 +87,8 @@ public class RegularExpression {
 
 				tweetIndex.add(tweetNode,"MessageID",tweetNode.getProperty("MessageID"));
 			}
-			else
-				System.out.println();
+			else;
+				//System.out.println();
 
 			tx.success();
 
@@ -94,8 +126,8 @@ public class RegularExpression {
 				userIndex=indexManager.forNodes("UserIndex");
 				userIndex.add(user, "UserNameKey", user.getProperty("UserNameKey"));
 			}
-			else
-				System.out.println();
+			else;
+				//System.out.println();
 			tx.success();
 		}
 		catch (Exception e) {
@@ -122,8 +154,8 @@ public class RegularExpression {
 				hashTagIndex.add(hashTagNode, "HashTagKey", hashTagNode.getProperty("HashTagKey"));
 
 			}
-			else
-				System.out.println();
+			else;
+				//System.out.println();
 			tx.success();
 		}
 		catch(Exception e){
@@ -158,7 +190,7 @@ public class RegularExpression {
 				relIndex.add(relation, "Contains", relation.getProperty("Contains"));
 			}
 			else if(relationshipname.equalsIgnoreCase("IsARetweetOf")){
-				relation=node1.createRelationshipTo(node2, RelationType.ISARETWEETOF);
+				relation=node1.createRelationshipTo(node2, RelationType.IS_A_RETWEETOF);
 				relation.setProperty("IsARetweetOf", "IsARetweetOf");
 				relIndex.add(relation,"IsARetweetOf",relation.getProperty("IsARetweetOf"));
 			}
@@ -173,7 +205,7 @@ public class RegularExpression {
 				relIndex.add(relation, "Replies", relation.getProperty("Replies"));
 			}
 			else if(relationshipname.equalsIgnoreCase("RepliesToTweet")){
-				relation=node1.createRelationshipTo(node2, RelationType.ISAREPLYTOTWEET);
+				relation=node1.createRelationshipTo(node2, RelationType.IS_A_REPLYTOTWEET);
 				relation.setProperty("RepliesToTweet", "RepliesToTweet");
 				relIndex.add(relation,"RepliesToTweet",relation.getProperty("RepliesToTweet"));
 			}
@@ -226,10 +258,117 @@ public class RegularExpression {
 			}
 		} );
 	}
+	public Map<JSONObject,JSONArray> query(String originalQuery){
+	    WebResource resource = Client.create().resource( "http://localhost:7474/db/data/cypher" );
+	    String query = JSONObject.escape(originalQuery);
+	    ClientResponse cypherResponse = resource.accept( MediaType.APPLICATION_JSON ).type( MediaType.TEXT_PLAIN )
+	              .entity( "{\"query\" : \""+query+"\", \"params\" : {}}" )
+	              .post( ClientResponse.class );
+	    String cypherResult = cypherResponse.getEntity( String.class );
+	//    System.out.println(cypherResult);
+	   // System.out.println();
+	    cypherResponse.close();
+	    //JSONObject send = new JSONObject();   
+	    ArrayList<JSONObject[]> rows= new ArrayList<JSONObject[]>();
+	    JSONObject obj = (JSONObject)JSONValue.parse(cypherResult);
+	    //System.out.println(obj);
+	    JSONArray data = (JSONArray)obj.get("data");
+	    System.out.println(data);
+//	    ArrayList<JSONObject> fields= new ArrayList<JSONObject>();	
+//	    System.out.println(data+" json data row size");
+//	    ArrayList<JSONObject> jsonField = new ArrayList<JSONObject>();
+	    JSONArray send  = new JSONArray();
+	    Map<JSONObject, JSONArray> toSend = new HashMap<JSONObject, JSONArray>();
+	    System.out.println(data.size());
+	    JSONObject jsonOriginal = new JSONObject();
+	    for(int x=0; x < data.size(); x++){
+	    	JSONArray fieldData = (JSONArray) data.get(x);
+//	    	System.out.println(fieldData);
+	    	System.out.println(fieldData.size());
+	    	//for(int i = 0;i<fieldData.size() ;i++){
+	    		jsonOriginal = (JSONObject) fieldData.get(0);
+	    		System.out.println(jsonOriginal);
+	    		JSONObject objSingle = (JSONObject) fieldData.get(1);
+	    	    //jsonField.add((JSONObject) objSinigle.get("data"));
+	    		//System.out.println(i);
+	    	    //System.out.println((JSONObject) objSinigle.get("data"));
+	    	    send.add((JSONObject) objSingle.get("data"));
+	    	//}
+	       // JSONObject field = (JSONObject)row.get(y);
+	      /*  JSONObject fieldData = (JSONObject)data.get("data");
+	        System.out.println(fieldData);
+	        fields.add(fieldData);
+	        //System.out.println(fieldData);
+	        //send.put(y,fieldData);
+	     System.out.println(send);
+	      //System.out.println(fields);
+	      JSONObject[] fieldsArray = new JSONObject[fields.size()];  
+	      fields.toArray(fieldsArray);
+	      //System.out.println(fieldsArray);
+	      rows.add(fieldsArray);*/
+	    }
+	    //System.out.println(send);
+	   /* int index =0 ;
+	    for(JSONObject json : jsonField ){
+	    	send.put(index, json);
+	    	index++;
+	    }*/
+	    //System.out.println(send);
+	    //for(JSONObject json : field)
+	    //System.out.println(send);
+	    toSend.put((JSONObject) jsonOriginal.get("data"), send);
+	    return toSend;
+	  }
+	public JSONObject getJsonFromMessageList(List<Long> messgIDList, String node, String relationship){
+
+		Map<Long, JSONObject> JsonResultList = new HashMap<>();
+		for (Iterator<Long> iterator = messgIDList.iterator(); iterator.hasNext();) {
+			Long singleMessgID = (Long) iterator.next();
+			
+//			String cypherQuery = "START n=node(*) WHERE n.MessageID=" + singleMessgID + 
+//					" MATCH n-[:" + relationship + "]-m RETURN DISTINCT m";
+			String cypherQuery = "START n=node(*) WHERE n.MessageID=" + singleMessgID + 
+					" MATCH n<-[r]->m RETURN DISTINCT n,m";
+//			String cypherQuery = "START n=node(*) WHERE n.MessageID=" + singleMessgID + " RETURN n";
+//			System.out.println(query);
+			Map<JSONObject, JSONArray> finalData = query(cypherQuery);
+			JSONObject nodeData = new JSONObject();
+			for (Map.Entry<JSONObject, JSONArray> m : finalData.entrySet()) {
+				nodeData.put("node", m.getKey());
+				nodeData.put("depends", m.getValue());
+			}
+			
+			JsonResultList.put(singleMessgID, nodeData);		
+			
+		}
+		JSONObject send = new JSONObject();
+		for(Map.Entry<Long, JSONObject> entry : JsonResultList.entrySet()) {
+			send.put(entry.getKey(), entry.getValue());
+		}
+		return send;
+			
+	}
+	
+	public void writeJsonToFile(JSONObject jObj) {
+		File file = new File("C:\\Users\\Rohit\\Desktop\\data.txt");
+		
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(jObj.toJSONString());
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		String dataSetPath;
 		RegularExpression schema=new RegularExpression();
-
 
 		try {
 			dataSetPath = "C:\\Users\\Rohit\\Desktop\\Neo4j\\obama_20121015_20121115.txt";
@@ -238,7 +377,7 @@ public class RegularExpression {
 			int i = 0;
 			String fileLine;
 			boolean isRetweet = false;
-			while((fileLine =file.readLine()) != null && i<10000  && !fileLine.equals("")){
+			while((fileLine =file.readLine()) != null && i<100  && !fileLine.equals("")){
 				String[] temp = fileLine.split("\\|");
 				if((temp.length == 9 || temp.length == 8)){
 					if(fileLine.contains("RT @")){
@@ -452,9 +591,9 @@ public class RegularExpression {
 				i++;
 				isRetweet = false;
 				
-				System.out.println(i);
+				//System.out.println(i);
 			}
-			System.out.println("The number of lines is:" + i);
+			//System.out.println("The number of lines is:" + i);
 			file.close();
 			schema.service.shutdown();
 		}
@@ -466,6 +605,19 @@ public class RegularExpression {
 			e.printStackTrace();
 			System.out.println("here in finally exception");
 		}
+		String query = "START n=node(*) RETURN n limit 10";
+		//System.out.println(schema.query(query));
+		//schema.query(query);
+		List<Long> messageIds = new ArrayList<Long>();
+		/*messageIds.add(267416374350053376);
+		messageIds.add(267416391370551296l);*/
+		messageIds.add(267416714092871680L);
+		messageIds.add(267416441786073088L);
+		JSONObject obj = schema.getJsonFromMessageList(messageIds, "Romeo", "CONTAINS");
+//		System.out.println(obj.toJSONString());
+		//JSONObject demo = new  JSONObject();
+		schema.writeJsonToFile(obj);
+		
 	}
 	@Override
 	protected void finalize() throws Throwable {
